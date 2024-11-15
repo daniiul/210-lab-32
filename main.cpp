6ce33377c3d8686f70126273a09d171a7b564f9d
daniiul <dmalakhov2005@gmail.com>
2024-11-14 19:05:03 -0800

    initial

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <deque>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include "Car.h"
+
+using namespace std;
+
+int main()
+{
+    int prob = rand() % 100 + 1;  // returns random number 1-100
+}
