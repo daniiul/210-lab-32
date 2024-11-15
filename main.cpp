a264baef2f297c070b2bf8c11996f416db71fd04
daniiul <dmalakhov2005@gmail.com>
2024-11-14 19:16:43 -0800

    working on probability, finished print statement for queue

--- main.cpp
+++ main.cpp
@@ -9,7 +9,32 @@
 
 using namespace std;
 
+const int INITIAL_QUE = 2;
+
+void print(deque<Car>& toll_booth);
+
 int main()
 {
+    deque<Car> toll_booth;
+    int i = 0;
+
+    cout << "Initial Que: " << endl;
+    while(i < INITIAL_QUE)
+    {
+        Car temp;
+        toll_booth.push_back(temp);
+        i++;
+    }
+
+    print(toll_booth);
+
     int prob = rand() % 100 + 1;  // returns random number 1-100
+}
+
+void print(deque<Car>& toll_booth)
+{
+    for(auto it : toll_booth)
+    {
+        it.print();
+    }
 }
