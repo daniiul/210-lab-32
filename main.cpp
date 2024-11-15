0d67f925d173be1defce3572909b8bf42325f509
daniiul <dmalakhov2005@gmail.com>
2024-11-14 19:27:17 -0800

    working on fixing format errors

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-const int INITIAL_QUE = 2;
+const int INITIAL_QUEUE = 2;
 
 void print(deque<Car>& toll_booth);
 
@@ -18,23 +18,48 @@ int main()
     deque<Car> toll_booth;
     int i = 0;
 
-    cout << "Initial Que: " << endl;
-    while(i < INITIAL_QUE)
+    cout << "Initial Queue: " << endl;
+    while(i < INITIAL_QUEUE)
     {
         Car temp;
         toll_booth.push_back(temp);
         i++;
     }
-
     print(toll_booth);
 
-    int prob = rand() % 100 + 1;  // returns random number 1-100
+    int time = 1;
+    while(!toll_booth.empty())
+    {
+        int prob = rand() % 100 + 1;  // returns random number 1-100
+        cout << "Time " << time << " operation: ";
+        if (prob <= 55)
+        {
+            cout << "Left lane: ";
+            toll_booth.front().print();
+            cout << endl;
+            toll_booth.pop_front();
+        }
+        else if (prob <= 44)
+        {
+            Car temp;
+            cout << "Joined lane: ";
+            temp.print();
+            cout << endl;
+            toll_booth.push_back(temp);
+        }
+        cout << endl;
+        print(toll_booth);
+        time++;
+    }
+
+
 }
 
 void print(deque<Car>& toll_booth)
 {
     for(auto it : toll_booth)
     {
+        cout << "     ";
         it.print();
     }
 }
